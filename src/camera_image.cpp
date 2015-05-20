53f22e201757a429e42f9903d80bed84769acb78
Arna Ghosh <arnatubaikgp@gmail.com>
2015-05-20 10:09:17 +0530

    modified camera_image.cpp to select most focused frame from a set of 6 frames and added the results from video of green slide using stepper motors and controller(partially working). Added ToDo and Progress files.

--- src/camera_image.cpp
+++ src/camera_image.cpp
@@ -10,6 +10,46 @@
 #include "opencv2/imgproc/imgproc.hpp"
 using namespace std;
 
+cv::Mat rgb2gray(cv::Mat img){
+	cv::Mat res(img.rows,img.cols,CV_8UC1,cvScalarAll(0));
+	for(int i=0;i<img.rows;i++){
+		for(int j=0;j<img.cols;j++){
+			res.at<uchar>(i,j)=(4*img.at<cv::Vec3b>(i,j)[0]+4*img.at<cv::Vec3b>(i,j)[1]+img.at<cv::Vec3b>(i,j)[2])/9;
+		}
+	}
+	return res;
+}
+
+int absol(int x){
+	if(x<0)return -x;
+	return x;
+}
+
+long sobel_sharpness(cv::Mat color){
+	long integral=0;
+	cv::Mat gray=rgb2gray(color);
+	cv::Mat sobel=gray.clone();
+	int max_abs=0;
+	for(int i=1;i<gray.rows-1;i++){
+		for(int j=1;j<gray.cols-1;j++){
+			int gx=(int)(gray.at<uchar>(i+1,j+1)+2*gray.at<uchar>(i,j+1)+gray.at<uchar>(i-1,j+1) - gray.at<uchar>(i-1,j-1)-2*gray.at<uchar>(i,j-1)-gray.at<uchar>(i+1,j-1));
+			int gy=(int)(gray.at<uchar>(i+1,j+1)+2*gray.at<uchar>(i+1,j)+gray.at<uchar>(i+1,j-1) - gray.at<uchar>(i-1,j-1)-2*gray.at<uchar>(i-1,j)-gray.at<uchar>(i-1,j+1));
+			integral+=(long)(absol(gx)+absol(gy))/2;
+			//int abs=sqrt(gx*gx+gy*gy);
+			int abs=(int)(absol(gx)+absol(gy))/2;
+			if(abs>max_abs)max_abs=abs;
+			sobel.at<uchar>(i,j)=abs;
+		}
+	}
+	for(int i=1;i<sobel.rows-1;i++){
+		for(int j=1;j<sobel.cols-1;j++){
+			sobel.at<uchar>(i,j)=sobel.at<uchar>(i,j)*255/max_abs;
+		}
+	}
+	return integral;
+}
+
+
 cv::Mat sharpenImage(cv::Mat img){
 	cv::Mat blurred=img;
 	//cv::GaussianBlur(img,blurred,cv::Size(5,5),5);
@@ -32,20 +72,35 @@ int main(int argc,char** argv){
 	cv::namedWindow("video",1);
 	cv::namedWindow("sharp",1);
 	string s="/home/arna/Desktop/IITKGP/projects/Microscope_working/";
-	//cv::VideoCapture cap(s.append(argv[1])); 
-	cv::VideoCapture cap(0);
+	int r=atoi(argv[1]);
+	//cv::VideoCapture cap(s.append(argv[2])); 
+	cv::VideoCapture cap(1);
 	long long int t=0;
+	int selection=0;
+	long long int sharpest=0;
+	cv::Mat sharpestIm;
 	while(1){
 		cv::Mat frame;
 		cap>>frame;
 		cv::imshow("video",frame);
 		frame=sharpenImage(frame);
 		cv::imshow("sharp",frame);
-		std::ostringstream oss;
-		oss<<"im"<<(t-20)<<".jpg";
-		//if(t>10 && t%10==0)cv::imwrite(oss.str(),frame);
+		long long sharpness=sobel_sharpness(frame);
+		//cout<<t<<":"<<selection<<" "<<sharpness<<" "<<sharpest<<endl;
+		if(sharpness>sharpest){
+			sharpest=sharpness;
+			frame.copyTo(sharpestIm);
+		}
+		if(selection>5){
+			std::ostringstream oss;
+			oss<<"im"<<(t-10)<<".jpg";
+			if(r==1 && t>10)cv::imwrite(oss.str(),sharpestIm);
+			sharpest=0;
+			selection=0;
+		}
 		if(cv::waitKey(33)==27)break;
 		t++;
+		selection++;
 	}
 	std::cout<<t<<std::endl;
 	return 0;
